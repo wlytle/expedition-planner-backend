a300473b5441e739c3c6dac62e54c14379bad9ba
wlytle <wlytle89@gmail.com>
2021-01-28 12:08:39 -0700

    allows trip to be created with no collaborators

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -36,8 +36,10 @@ class TripsController < ApplicationController
     # create usertrip and assign current suer as cretor of this trip
     @trip.user_trips.build(user: current_user, accepted: true, created: true)
     # create usertrips for invited collaborators
-    params[:trip][:collabs].each do |collab|
-      @trip.user_trips.build(user_id: collab[:id], accepted: false, created: false)
+    if params[:collabs]
+      params[:trip][:collabs].each do |collab|
+        @trip.user_trips.build(user_id: collab[:id], accepted: false, created: false)
+      end
     end
 
     if @trip.save
